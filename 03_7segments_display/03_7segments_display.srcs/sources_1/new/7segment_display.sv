d29aaeb70a04d75aa12f1433129cdbc1c824b178
Mahmoud Albisary <mahmoudalbisary@icloud.com>
2026-01-11 22:04:48 +0100

    Create 03_7segment_display project
    - add 7segment_display module with initial structure

---
+++ 03_7segments_display/03_7segments_display.srcs/sources_1/new/7segment_display.sv
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/11/2026 10:01:35 PM
+// Design Name: 
+// Module Name: 7segment_display
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module segment_display(
+    );
+endmodule
