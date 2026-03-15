88c43c21abdfeb77e86c90e9561fd6f38cfb9298
Mahmoud Albisary <mahmoudalbisary@icloud.com>
2026-03-15 14:49:30 +0100

    Add commented-out code for segment display implementation in 7segment_display.sv just to show a simple example how the 7 segment display works

--- 03_7segments_display/03_7segments_display.srcs/sources_1/new/7segment_display.sv
+++ 03_7segments_display/03_7segments_display.srcs/sources_1/new/7segment_display.sv
@@ -19,7 +19,23 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+//The code below shows how to turn on one display for the first two segments
+//module segment_display(
+//    output logic [3:0] an ,
+//    output logic [6:0] c ,
+//    output logic dp
+//    );
+    
+//    always_comb begin
+//        an = 4'b1110;     // enable digit 0 to enable the first display
+//        c  = 7'b1001111;  // pattern for "1" (active low)
+//        dp = 1'b1;        // decimal point off
+//        //Note in the code above we can see that the segments, decimal point and anodes are active on low (active when 0)
+//    end
+//endmodule
+
 
 module segment_display(
     );
+
 endmodule
