180b80b54907d3e52c237bc61d6b49907ed7ba13
Mahmoud Albisary <mahmoudalbisary@icloud.com>
2026-03-16 02:00:55 +0100

    Start the clock project
    - Show the initial values for the clock (all zeros)
    
    - Add num_to_display module for 7-segment display logic

--- 03_7segments_display/03_7segments_display.srcs/sources_1/new/7segment_display.sv
+++ 03_7segments_display/03_7segments_display.srcs/sources_1/new/7segment_display.sv
@@ -34,8 +34,32 @@
 //    end
 //endmodule
 
+module num_to_display(
+    input logic num[3:0];
+    output logic display[6:0];
+)
+    case(num)
+        4'd0: display = 7'b1000000;  
+        4'd1: display = 7'b1111001;  
+        4'd2: display = 7'b0100100;  
+        4'd3: display = 7'b0110000;  
+        4'd4: display = 7'b0011001;  
+        4'd5: display = 7'b0010010;  
+        4'd6: display = 7'b0000010;  
+        4'd7: display = 7'b1111000;  
+        4'd8: display = 7'b0000000;  
+        4'd9: display = 7'b0010000;
+endmodule
 
 module segment_display(
+    output logic [3:0] an ,
+    output logic [6:0] c ,
+    output logic dp
     );
+    always_comb begin
+        an = 4'b0000;     // enable all displays
+        c  = 7'b1000000;  // pattern for "0" for all displays
+        dp = 1'b0;        // decimal point ON
 
+    end
 endmodule
