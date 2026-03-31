740b890610d0d7b80a8db354fc8fba3bb5e1f8e3
Mahmoud Albisary <mahmoudalbisary@icloud.com>
2026-04-01 00:53:45 +0200

    Fix num_to_display function to correctly assign display values and enhance blink_display module functionality

--- 03_7segments_display/03_7segments_display.srcs/sources_1/new/7segment_display.sv
+++ 03_7segments_display/03_7segments_display.srcs/sources_1/new/7segment_display.sv
@@ -37,16 +37,39 @@
 function automatic logic [6:0] num_to_display(input logic [3:0] num);
     begin
     case(num)
-        4'd0: display = 7'b1000000;  
-        4'd1: display = 7'b1111001;  
-        4'd2: display = 7'b0100100;  
-        4'd3: display = 7'b0110000;  
-        4'd4: display = 7'b0011001;  
-        4'd5: display = 7'b0010010;  
-        4'd6: display = 7'b0000010;  
-        4'd7: display = 7'b1111000;  
-        4'd8: display = 7'b0000000;  
-        4'd9: display = 7'b0010000;
+            4'd0: num_to_display = 7'b1000000;
+            4'd1: num_to_display = 7'b1111001;
+            4'd2: num_to_display = 7'b0100100;
+            4'd3: num_to_display = 7'b0110000;
+            4'd4: num_to_display = 7'b0011001;
+            4'd5: num_to_display = 7'b0010010;
+            4'd6: num_to_display = 7'b0000010;
+            4'd7: num_to_display = 7'b1111000;
+            4'd8: num_to_display = 7'b0000000;
+            4'd9: num_to_display = 7'b0010000;
+        endcase
+    end
+endfunction
+
+module blink_display(
+    input logic clk, // clock
+    input logic rst, // reset
+    output logic blink
+);
+    logic [25:0] count;
+    always_ff @(posedge clk or posedge rst) begin
+        if(rst) begin
+            count <= 26'd0;
+            blink <= 1'b1 ;
+        end else begin
+            if(count == 26'd50000000) begin
+                count <= 26'd0;
+                blink <= ~blink;
+            end else begin
+                count <= count + 1;
+            end
+        end
+    end
 endmodule
 
 module segment_display(
