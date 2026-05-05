7bf74107ca155577ee0dab7f8a0bdcb270aba75e
Mahmoud Albisary <mahmoudalbisary@icloud.com>
2026-05-05 20:28:53 +0200

    Divide the SET_VALUE stage to SET_MINUTES and SET_SECONDS

--- 05_time_counter/05_time_counter.srcs/sources_1/new/timer_states_pkg.sv
+++ 05_time_counter/05_time_counter.srcs/sources_1/new/timer_states_pkg.sv
@@ -22,7 +22,8 @@
 typedef enum logic [2:0] {
     IDLE,       // waiting / showing default screen
     SET_MODE,   // choose timer, stopwatch, event counter, etc.
-    SET_VALUE,  // set countdown start value or counter target
+    SET_SECONDS,  // set countdown start value or counter target
+    SET_MINUTES,  // set countdown start value or counter target
     RUN,        // actively counting
     PAUSE,      // frozen count, can resume/reset
     DONE        // countdown finished / target reached
